c1e09d3678a098260df7d1833a1e7e0ca7c2ca15
Will Cooley <willdaplum@gmail.com>
2025-04-11 21:07:47 -0500

    test displaying cloudfront served video

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+// console.log("before any imports", typeof useRef);
+import { useState, useEffect, useRef } from "react";
 import {
   Button,
   Heading,
@@ -12,14 +13,52 @@ import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
+import Hls from "hls.js";
 /**
  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
  */
+console.log("after imports", typeof useRef);
 
 Amplify.configure(outputs);
 const client = generateClient({
   authMode: "userPool",
 });
+
+function VideoWithHLS() {
+  const videoRef = useRef(null);
+  console.log("videoRef", videoRef);
+  console.log("typeof videoRef", typeof videoRef);
+  useEffect(() => {
+    const hls = new Hls({
+      debug: true,
+    });
+
+    if (Hls.isSupported() && videoRef.current) {
+      hls.loadSource("https://dxp9j1kps1txn.cloudfront.net/ccd81920-fba7-419c-a6dd-b9a3733e94f9/AppleHLS1/IMG_2565_Ott_Hls_Ts_Avc_Aac_16x9_960x540p_3.5Mbps_qvbr.m3u8");
+      hls.attachMedia(videoRef.current);
+      hls.on(Hls.Events.ERROR, (err) => {
+        console.log(err);
+      });
+    } else {
+      console.log("load");
+    }
+    return () => {
+      // cleanup (when component destroyed or when useEffect runs twice on StrictMode)
+      hls.destroy();
+    };
+  }, []);
+
+  // console.log("typeof videoRef", typeof videoRef);
+  // console.log("videoRef", videoRef);
+  return (
+    <video
+      ref={videoRef}
+      controls
+      autoPlay
+      style={{ width: "250px", borderRadius: "10px" }}
+    />
+  );
+}
 
 export default function App() {
   const [userprofiles, setUserProfiles] = useState([]);
@@ -71,7 +110,8 @@ export default function App() {
             </View>
           </Flex>
         ))}
-      </Grid>
+      </Grid> 
+      <VideoWithHLS />
       <Button onClick={signOut}>Sign Out</Button>
     </Flex>
   );
