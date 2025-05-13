d1bcee6d059b6e9b839406e3e6e073480f180387
willdaplum <willdaplum@gmail.com>
2025-05-13 14:02:14 -0500

    STREAM-8 scaffold about page

--- src/ProductInfo.jsx
+++ src/ProductInfo.jsx
@@ -1,22 +1,23 @@
 export default function ProductInfo() {
+  const title = "Featured title";
+  const description =
+    "Paragraph of text beneath the heading to explain the heading.";
   return (
-    <div className="card mb-3">
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <h6 className="card-subtitle mb-2 text-body-secondary">
-          Card subtitle
-        </h6>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card content.
-        </p>
-        <a href="#" className="card-link">
-          Card link
-        </a>
-        <a href="#" className="card-link">
-          Another link
-        </a>
+      <div className="col d-flex align-items-start">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-app"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4z" />
+          </svg>
+          <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">{title}</h3>
+          <p>{description}</p>
+        </div>
       </div>
-    </div>
   );
 }
