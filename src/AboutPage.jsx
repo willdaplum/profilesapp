d87b90f071f148e93c247349da78d1410f0387cc
William Emory Cooley <wcooley@umich.edu>
2025-05-20 13:48:38 -0500

    STREAM-8 improve about page layout

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,106 +1,29 @@
 import NavBar from "./NavBar.jsx";
 import ProductInfo from "./ProductInfo.jsx";
+import SvgElement from "./SvgElement.jsx";
+import FaqAccordion from "./FaqAccordion.jsx";
 
 export default function AboutPage() {
   return (
     <>
+      <SvgElement />
       <NavBar />
-      <div className="container px-4 py-5" id="icon-grid">
+      <div className="b-example-divider"></div>
+      <div className="container px-4 pt-5" id="icon-grid">
         <h2 className="pb-2 border-bottom">Icon grid</h2>
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
-          <div className="col d-flex align-items-start">
-            <ProductInfo />
-            <ProductInfo />
-            <ProductInfo />
-            <ProductInfo />
-            <ProductInfo />
-            <ProductInfo />
-            <ProductInfo />
-            <ProductInfo />
-          </div>
+          <ProductInfo />
+          <ProductInfo />
+          <ProductInfo />
+          <ProductInfo />
+          <ProductInfo />
+          <ProductInfo />
+          <ProductInfo />
+          <ProductInfo />
         </div>
       </div>
-      <div className="accordion accordion-flush" id="accordionFlushExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseOne"
-              aria-expanded="false"
-              aria-controls="flush-collapseOne"
-            >
-              Accordion Item #1
-            </button>
-          </h2>
-          <div
-            id="flush-collapseOne"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionFlushExample"
-          >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> class. This is the
-              first items accordion body.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseTwo"
-              aria-expanded="false"
-              aria-controls="flush-collapseTwo"
-            >
-              Accordion Item #2
-            </button>
-          </h2>
-          <div
-            id="flush-collapseTwo"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionFlushExample"
-          >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> class. This is the
-              second items accordion body. Lets imagine this being filled with
-              some actual content.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseThree"
-              aria-expanded="false"
-              aria-controls="flush-collapseThree"
-            >
-              Accordion Item #3
-            </button>
-          </h2>
-          <div
-            id="flush-collapseThree"
-            className="accordion-collapse collapse"
-            data-bs-parent="#accordionFlushExample"
-          >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> class. This is the
-              third items accordion body. Nothing more exciting happening here
-              in terms of content, but just filling up the space to make it
-              look, at least at first glance, a bit more representative of how
-              this would look in a real-world application.
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="container"><FaqAccordion /></div>
+      
     </>
   );
 }
--- src/ProductInfo.jsx
+++ src/ProductInfo.jsx
@@ -3,21 +3,19 @@ export default function ProductInfo() {
   const description =
     "Paragraph of text beneath the heading to explain the heading.";
   return (
-      <div className="col d-flex align-items-start">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-app"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 2a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3zM5 1a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4z" />
-          </svg>
-          <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">{title}</h3>
-          <p>{description}</p>
-        </div>
+    <div className="col d-flex align-items-start">
+      <svg
+        className="bi text-body-secondary flex-shrink-0 me-3"
+        width="1.75em"
+        height="1.75em"
+        aria-hidden="true"
+      >
+        <use xlinkHref="#bootstrap"></use>
+      </svg>
+      <div>
+        <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">{title}</h3>
+        <p>{description}</p>
       </div>
+    </div>
   );
 }
